098b88ce7e990600ef23b312e1cd1120dede802d
martinperez2306 <perezmartincontacto@gmail.com>
2018-11-15 09:35:58 -0300

    Agrego Servicio de Empleados

--- aninfo/src/main/java/com/fiuba/aninfo/controller/EmpleadosController.java
+++ aninfo/src/main/java/com/fiuba/aninfo/controller/EmpleadosController.java
@@ -1,5 +1,28 @@
 package com.fiuba.aninfo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fiuba.aninfo.model.Empleado;
+import com.fiuba.aninfo.service.EmpleadoService;
+
+@Controller
 public class EmpleadosController {
 
+	@Autowired
+	private EmpleadoService empleadoService;
+	
+	@RequestMapping(value="/hour", method = RequestMethod.GET)
+	public @ResponseBody ResponseEntity<List<Empleado>> getHour() {
+		List<Empleado> proyectos = this.empleadoService.getAllEmpleados();
+		ResponseEntity<List<Empleado>> response = new ResponseEntity<List<Empleado>>(proyectos,HttpStatus.OK);
+		return response;
+	}
 }
---
+++ aninfo/src/main/java/com/fiuba/aninfo/service/EmpleadoService.java
@@ -0,0 +1,17 @@
+package com.fiuba.aninfo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.fiuba.aninfo.model.Empleado;
+
+@Service
+public class EmpleadoService {
+
+	public List<Empleado> getAllEmpleados() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
