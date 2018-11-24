b36240c988863b5d0ffa1a2ba050145a5a1d9824
martinperez2306 <perezmartincontacto@gmail.com>
2018-11-24 14:00:59 -0300

    Agrego rol de empleado

---
+++ aninfo/src/main/java/com/fiuba/aninfo/model/Rol.java
@@ -0,0 +1,29 @@
+package com.fiuba.aninfo.model;
+
+public class Rol {
+	
+	private String rol;
+	
+	private String url;
+	
+	public Rol() {
+		
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
