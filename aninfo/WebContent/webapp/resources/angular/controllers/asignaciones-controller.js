0922d055a23d4986227d8d2a693ddb14a93dd589
martinperez2306 <perezmartincontacto@gmail.com>
2018-11-25 07:49:50 -0300

    alta baja asignaciones

--- aninfo/WebContent/webapp/resources/angular/controllers/asignaciones-controller.js
+++ aninfo/WebContent/webapp/resources/angular/controllers/asignaciones-controller.js
@@ -1,4 +1,4 @@
-var app = angular.module('aninfoApp', [,'empleadosModule','tareasModule']);
+var app = angular.module('aninfoApp', ['proyectosModule','empleadosModule','tareasModule']);
 var scope = {};
 
 app.factory('asignacionesFunctions',function($http){
@@ -16,7 +16,7 @@ app.factory('asignacionesFunctions',function($http){
 		$http({
 			method:'POST',
 			url: '/aninfo/asignaciones',
-			data:proyecto
+			data:asignacion
 		}).then(function(response){
 			callback(response);
 		})
@@ -25,7 +25,7 @@ app.factory('asignacionesFunctions',function($http){
 	var deleteAsignacion = function(asignacion,callback){
 		$http({
 			method:'DELETE',
-			url: '/aninfo/asignacion/' + asignacion.id
+			url: '/aninfo/asignaciones/' + asignacion.id
 		}).then(function(response){
 			callback(response);
 		})
@@ -37,23 +37,83 @@ app.factory('asignacionesFunctions',function($http){
 		deleteAsignacion:deleteAsignacion
 	}
 })
-.controller('asignacionesController', function($scope,$location,empleadosFunctions,tareasFunctions) {
+.controller('asignacionesController', function($scope,$location,asignacionesFunctions,proyectosFunctions,empleadosFunctions,tareasFunctions) {
     scope = $scope;
     
+    $scope.codigoProyecto = $location.absUrl().split("/")[6];
     $scope.codigoTarea = $location.absUrl().split("/")[7];
     
+    $scope.proyecto = {};
     $scope.tarea = {};
-    $scope.empleados = [];
+    $scope.asignaciones = [];
+    $scope.empleadosDisponibles = [];
+    
+    NuevaAsignacion = function(){
+    	var self =this;
+    	self.codigoProyecto = $scope.proyecto.codigo;
+    	self.codigoTarea = $scope.tarea.nombre;
+    	self.dniEmpleado="";
+    	self.edit = false;
+    }
+    
+    Asignacion = function(nuevaAsignacion){
+    	var self =this;
+    	self.id = 0;
+    	self.codigoProyecto = nuevaAsignacion.codigoProyecto;
+    	self.codigoTarea = nuevaAsignacion.codigoTarea;
+    	self.dniEmpleado= nuevaAsignacion.dniEmpleado;
+    }
     
     $scope.init = function(){
     	
-    	tareasFunctions.getTarea($scope.codigoTarea,function(response){
-    		scope.tarea = response.data;
+    	proyectosFunctions.getProyecto($scope.codigoProyecto,function(response){
+    		$scope.proyecto = response.data;
     	})
     	
+    	tareasFunctions.getTarea($scope.codigoTarea,function(response){
+    		$scope.tarea = response.data;
+    	})
+    	
+    	asignacionesFunctions.getAsignaciones(function(response){
+    		angular.forEach(response.data,function(asignacion){
+    			$scope.asignaciones.push(asignacion);
+    		})
+    	})
+    	
+    }
+    
+    $scope.agregarAsignacion = function(){
     	empleadosFunctions.getDesarrolladores(function(response){
     		angular.forEach(response.data, function(empleado){
-    			$scope.empleados.push(empleado);
+    			$scope.empleadosDisponibles.push(empleado);
+    		})
+    	})
+    	$scope.nuevaAsignacion = new NuevaAsignacion();
+    	$scope.nuevaAsignacion.edit = true;
+    }
+    
+    $scope.aceptarNuevaAsignacion = function(){
+    	var asignacion = new Asignacion($scope.nuevaAsignacion);
+    	asignacionesFunctions.addAsignacion(asignacion,function(response){
+    		$scope.nuevaAsignacion = new NuevaAsignacion();
+    	})
+    }
+    
+    $scope.cancelarNuevaAsignacion = function(){
+    	$scope.nuevaAsignacion.edit = false;
+    }
+    
+    $scope.borrarAsignacion = function(asignacion){
+    	asignacionesFunctions.deleteAsignacion(asignacion,function(response){
+    		$scope.cargarAsignaciones();
+    	})
+    }
+    
+    $scope.cargarAsignaciones = function(){
+    	$scope.asignaciones = [];
+    	asignacionesFunctions.getAsignaciones(function(response){
+    		angular.forEach(response.data, function(asignacion){
+    			$scope.asignaciones.push(asignacion);
     		})
     	})
     }
