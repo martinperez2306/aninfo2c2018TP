6f271d6248108bea30ad33ceb8106711b384d7d1
martinperez2306 <perezmartincontacto@gmail.com>
2018-11-25 07:16:03 -0300

    controler de angular para asignaciones

--- aninfo/WebContent/webapp/resources/angular/controllers/asignaciones-controller.js
+++ aninfo/WebContent/webapp/resources/angular/controllers/asignaciones-controller.js
@@ -1,7 +1,43 @@
 var app = angular.module('aninfoApp', [,'empleadosModule','tareasModule']);
 var scope = {};
 
-app.controller('asignacionesController', function($scope,$location,empleadosFunctions,tareasFunctions) {
+app.factory('asignacionesFunctions',function($http){
+	
+	var getAsignaciones = function(callback){
+		$http({
+			method: 'GET',
+			url: '/aninfo/asignaciones'
+		}).then(function(response){
+			callback(response);
+		})
+	}
+	
+	var addAsignacion = function(asignacion,callback){
+		$http({
+			method:'POST',
+			url: '/aninfo/asignaciones',
+			data:proyecto
+		}).then(function(response){
+			callback(response);
+		})
+	}
+	
+	var deleteAsignacion = function(asignacion,callback){
+		$http({
+			method:'DELETE',
+			url: '/aninfo/asignacion/' + asignacion.id
+		}).then(function(response){
+			callback(response);
+		})
+	}
+	
+	return{
+		getAsignaciones:getAsignaciones,
+		addAsignacion:addAsignacion,
+		deleteAsignacion:deleteAsignacion
+	}
+})
+.controller('asignacionesController', function($scope,$location,empleadosFunctions,tareasFunctions) {
     scope = $scope;
     
     $scope.codigoTarea = $location.absUrl().split("/")[7];
